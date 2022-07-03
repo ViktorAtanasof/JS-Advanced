4ad1d9dec5ad5763088d35aa5cabc9c3671b7f8c
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-07-03 11:17:50 +0300

    Create showMore.js

---
+++ DOM Introduction - Lab/04. Show More/showMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const hideLink = document.getElementById('more');
+    hideLink.style.display = 'none';
+    const showMoreInfo = document.getElementById('text');
+    showMoreInfo.style.display = 'inline';
+}
