132c9906b05ce1ff51f1c2a7a2743ae0e75a900b
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-07-23 14:24:50 +0300

    Create sortArray.js

---
+++ Advanced Functions - Exercise/sortArray.js
@@ -0,0 +1,10 @@
+function sortArray(arr, string) {
+    if (string === "asc") {
+        arr.sort((a, b) => a - b);
+    } else {
+        arr.sort((a, b) => b - a);
+    }
+    return arr;
+}
+
+sortArray([14, 7, 17, 6, 8], "desc");
